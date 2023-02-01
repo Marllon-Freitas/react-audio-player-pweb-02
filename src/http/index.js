69be4f7c005fa4e8e37d9b1e41025b7f3f92a575
Marllon Freitas <marllonfreitas64@gmail.com>
2023-02-01 13:22:34 -0300

    Initial Commit

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://api.napster.com/v2.2/";
+const BASE_URL = "https://api.napster.com/v2.2/";
 
 const http = axios.create({
   baseURL: BASE_URL,
